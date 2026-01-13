f7649a34ea0daddf21900dcd88dced82898143e8
henriquepierandrei <henriquepierandrei1@gmail.com>
2026-01-13 20:56:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-amber-400'>adsa</div>
+  )
+}
+
+export default App
